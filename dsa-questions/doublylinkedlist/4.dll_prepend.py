7f2166d2f09b61ca6bb89d16d0de802994b2eab9
iamsamiofficial <muazzemhussainkhan@gmail.com>
2023-07-30 15:04:08 +0600

    prepend

---
+++ dsa-questions/doublylinkedlist/4.dll_prepend.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class Doublylinkedlist:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    
+    def printlist(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length +=1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return  None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -=1
+        return temp
+
+    def prepend(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length +=1
+        return True
+
+doubly = Doublylinkedlist(1)
+doubly.append(2)
+doubly.append(3)
+doubly.prepend(0 )
+doubly.printlist()
