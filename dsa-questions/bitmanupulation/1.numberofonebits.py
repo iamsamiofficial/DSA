7a90120856b9f2a4ff5e00bc6d701de91bd08d3b
iamsamiofficial <muazzemhussainkhan@gmail.com>
2023-12-19 18:17:58 +0600

    numberofonebits

---
+++ dsa-questions/bitmanupulation/1.numberofonebits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        # while n:
+        #     res+=n%2
+        #     n = n >> 1
+        # return res
+        while n:
+            n &=(n-1)
+            res+=1
+        return res
+        
